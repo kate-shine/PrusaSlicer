4016d9cca343e42e50e12bec7e778e2af5f2c6c0
Lukas Matena <lukasmatena@seznam.cz>
2020-03-06 14:08:55 +0100

    Revert "Fixed encoding in 'empty layers detected' error message"
    This reverts commit f3ce3bfb8c499c91a112417cc62352dbecaf2394.

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -637,18 +637,11 @@ std::vector<GCode::LayerToPrint> GCode::collect_layers_to_print(const PrintObjec
                                || (layer_to_print.support_layer && layer_to_print.support_layer->has_extrusions());
 
             if (has_extrusions && layer_to_print.print_z() > maximal_print_z + 2. * EPSILON)
-                throw std::runtime_error((boost::format("%1%\n\n%2%: %3%\n%4%: %5%\n\n%6%")
-                             % _(L("Empty layers detected, the output would not be printable."))
-                             % _(L("Object name"))
-                             % object.model_object()->name
-                             % _(L("Print z"))
-                             % std::to_string(layers_to_print.back().print_z())
-                             % _(L("This is usually caused by negligibly small "
-                                 "extrusions or by a faulty model. Try to repair "
-                                 "the model or change its orientation on the bed."))
-                             ).str());
-
-
+                throw std::runtime_error(_(L("Empty layers detected, the output would not be printable.")) + "\n\n" +
+                    _(L("Object name")) + ": " + object.model_object()->name + "\n" + _(L("Print z")) + ": " +
+                    std::to_string(layers_to_print.back().print_z()) + "\n\n" + _(L("This is "
+                    "usually caused by negligibly small extrusions or by a faulty model. Try to repair "
+                    "the model or change its orientation on the bed.")));
             // Remember last layer with extrusions.
             last_extrusion_layer = &layers_to_print.back();
         }
